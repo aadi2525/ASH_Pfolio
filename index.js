ce9366ff4f986c3111f3445df609cce759c194f0
adiraj hodlurkar <123805819+aadi2525@users.noreply.github.com>
2023-04-14 21:04:43 +0530

    update 1

---
+++ index.js
@@ -0,0 +1,15 @@
+let scrolldown = document.getElementById("s_down").onclick=function(){
+    location.href="#qualifications";
+};
+let askmail =document.getElementById("mail").onclick=function(){
+    alert(" mail sended !");
+}
+let d_cv =document.getElementById("download").onclick=function(){
+    alert("CV Downloded");
+}
+
+let logoo =document.getElementById("logo").onclick=function(){
+    location.href="#header";
+  
+    
+}
